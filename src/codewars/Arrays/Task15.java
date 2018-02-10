21cf1c2e5096c1c0a3f60a7577a64d5890da470a
Natali <tvnatasha0310@gmail.com>
2018-02-10 15:24:30 +0100

    new

---
+++ src/codewars/Arrays/Task15.java
@@ -0,0 +1,14 @@
+package codewars.Arrays;
+
+public class Task15 {
+
+//https://www.codewars.com/kata/57eae65a4321032ce000002d/train/java
+
+
+public static String fakeBin(String numberString) {
+    return "";
+    //return numberString.replaceAll("[1-4]", "0").replaceAll("[5-9]", "1");
+
+    public static void main (String[]args){
+    }
+}}
