af13da977b65e6e549a3cee119951cfba23417f4
Natali <tvnatasha0310@gmail.com>
2018-03-29 15:56:04 +0200

    new

---
+++ src/homeworks/code/Task1.java
@@ -0,0 +1,32 @@
+package homeworks.code;
+
+public class Task1 {
+    public static void main(String [] args) {
+        String name = "Clint";
+        int age = 50;
+        int iq = age;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(iq);
+    }
+}
+//operators
+// == (compare),
+// != (not equal),
+// >, <, >=, <=
+
+// && (and),
+// || (or)
+
+//&&
+//true && true -> true
+//true && false -> false
+//false && true -> false
+//false && false -> false
+
+//||
+//true || true -> true
+//true || false -> true
+//false || true -> true
+//false || false -> false
+
