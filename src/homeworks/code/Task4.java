57d8ccbd085413d9c105e176f9f08852b4a1c22d
Natali <tvnatasha0310@gmail.com>
2018-04-01 15:22:45 +0200

    new

---
+++ src/homeworks/code/Task4.java
@@ -0,0 +1,13 @@
+package homeworks.code;
+
+public class Task4 {
+    public static void main(String[] args) {
+        String name;
+        String lastname;
+        name = "Bob";
+        lastname = "Jones";
+
+        System.out.println (name);
+        System.out.println(lastname);
+    }
+}
