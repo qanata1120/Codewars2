b8570333bb6718cb0a6a8f95bbe2587f326742fa
Natali <tvnatasha0310@gmail.com>
2020-07-11 18:15:05 +0200

    new

---
+++ src/codewars/Arrays/Task22.java
@@ -0,0 +1,14 @@
+package codewars.Arrays;
+
+public class Task22 {
+    public static void main(String[]args) {
+        int[] a = {1, 2, 3, 4, 5, 6}; //arrays
+        int[] b = new int[100];
+        int[] c = new int[]{10};
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum = sum + a[i];   //is the same  to    sum += a[i]
+        }
+        System.out.print(sum);
+    }}
+}
