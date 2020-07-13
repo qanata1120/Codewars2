427dfa4186c517b6ae7c04172f29c3282f05de61
Natali <tvnatasha0310@gmail.com>
2020-07-13 18:17:06 +0200

    new

---
+++ src/codewars/Arrays/Task23.java
@@ -0,0 +1,17 @@
+package codewars.Arrays;
+
+public class Task23 {
+
+    public static void main(String[]args) {
+        int[] a = {5, 7, 2, 3, 4, 5, 6}; //arrays
+        int max =0;
+        //  int index = 0;
+        for(int i = 0; i<a.length; i++){
+            if (max < a[i]){
+                max = a[i];
+                //     index =i;
+            }}
+        System.out.println(max);
+        //  System.out.println(index);
+    }}
+
