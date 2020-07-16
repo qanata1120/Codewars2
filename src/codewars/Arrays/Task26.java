ab87cb49d34ea7c1bab957f15690743abb1f2967
Natali <tvnatasha0310@gmail.com>
2020-07-16 10:10:14 +0200

    new

---
+++ src/codewars/Arrays/Task26.java
@@ -0,0 +1,15 @@
+package codewars.Arrays;
+
+public class Task26 {
+    public static void main(String[] args) {
+        int[] a = {-5, -7, -2, -3, -4, 5, 6};
+        int min = [0];   // Integer.MAX_VALUE;
+
+        for (int i = 0; i < a.length; i++) {
+            if (min < a[i]) {
+                min = a[i];
+            }
+        }
+        System.out.println(min);
+    }
+}
