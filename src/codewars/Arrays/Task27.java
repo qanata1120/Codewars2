8c95a6d076ea8fc6eed61d1b6aeaeb3c31add8c8
Natali <tvnatasha0310@gmail.com>
2020-07-22 10:11:56 +0200

    new

---
+++ src/codewars/Arrays/Task27.java
@@ -0,0 +1,21 @@
+package codewars.Arrays;
+
+import java.util.Arrays;
+
+public class Task27 {
+    public static void main(String[]args) {
+        int[] a = {5, 7, 3, 2, 7, 6};
+        int[] b = {1, 2, 7, 2, 5, 3};
+        int[] x = new int[a.length + b.length];
+        System.out.println(Arrays.toString(x));
+        for (int i = 0; i <a.length + b.length; i++){
+            if (i < a.length) {
+                x[i] = a[i];
+            }  else {
+                x[i] = b[i - a.length];
+            }
+        }
+        System.out.println(Arrays.toString(x));   // we filled array x with values from array a and array b
+    }
+
+}
