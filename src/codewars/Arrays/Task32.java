0414033c9d735ccd0a0eb629dff5edf888e9f9af
Natali <tvnatasha0310@gmail.com>
2020-07-27 07:26:29 +0200

    new

---
+++ src/codewars/Arrays/Task32.java
@@ -0,0 +1,15 @@
+package codewars.Arrays;
+
+import java.util.Arrays;
+
+public class Task32 {
+    public static void main(String[]args) {
+        int[] x = {5, 1};
+        int[][] a = {x, {7, 5, 6}};
+        System.out.println(Arrays.deepToString(a));
+        a[0] = new int[]{1, 2, 3};
+        System.out.println(Arrays.deepToString(a));
+    }           //output [[5, 1], [7, 5, 6]]
+                // [[1, 2, 3], [7, 5, 6]]*/
+}
+
