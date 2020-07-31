678231b6e2c2bdda9c38d79152c396d8b13fc5da
Natali <tvnatasha0310@gmail.com>
2020-07-31 20:32:41 +0200

    new

---
+++ src/codewars/Arrays/Task33.java
@@ -0,0 +1,18 @@
+package codewars.Arrays;
+
+import java.util.Arrays;
+
+public class Task33 {
+    public static void main(String[]args) {
+        int [][] a = {
+                {5, 1},
+                {7, 5, 6} };
+        System.out.println(Arrays.deepToString(a));
+        int[] x = a[0];
+        x[0] = 100;
+        System.out.println(Arrays.deepToString(a));
+// output [[5, 1], [7, 5, 6]]
+//[[100, 1], [7, 5, 6]]
+    }
+
+}
