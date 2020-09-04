a2b73b6a263200c87533ce8562f7754f3d7332dc
Natali <tvnatasha0310@gmail.com>
2020-09-04 12:18:57 +0200

    2 new

---
+++ src/homeworks/code/Task5.java
@@ -0,0 +1,10 @@
+package homeworks.code;
+
+public class Task5 {
+    public static void main(String[] args) {
+
+        System.out.println("Good morning");
+        System.out.println("Good afternoon" + " " + "Good evening");
+        System.out.println("Good night." + " I am going to sleep");
+    }
+}
---
+++ src/homeworks/code/Task6.java
@@ -0,0 +1,27 @@
+package homeworks.code;
+
+public class Task6 {
+    public static void main(String[] args) {
+        char ch;
+        ch = '\u263a';
+        ch = '\u263b';
+        String ab = "\u27A1\uDF00";
+        String w = "\ud83c\udf7e";
+        String k = "\ud83D\uDE0A";
+        String e = "\ud83d\udc4d";
+
+        System.out.println(ch);
+        System.out.println(ab);
+        System.out.println(w);
+        System.out.println('\u2764');
+        System.out.println("\uD83D\uDE0A");
+        System.out.println(k);
+        System.out.println(e);
+        System.out.println("\ud83d\udc4d");
+        System.out.println('\u2705');
+        System.out.println("\ud83e\udd96");
+        System.out.println('\u0ca2');
+        System.out.println("\ud83c\udf54");
+    }
+
+}
