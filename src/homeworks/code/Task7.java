178f1e4a20a116b7549cbec3f9a185d1a0806989
Natali <tvnatasha0310@gmail.com>
2020-09-06 12:19:49 +0200

    new

---
+++ src/homeworks/code/Task7.java
@@ -0,0 +1,22 @@
+package homeworks.code;
+
+public class Task7 {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 5;
+        int result1 = a + b;
+        int result2 = a - b;
+        int result3 = a * b;
+        int result4 = a / b;
+        int result5 = a % 3;
+
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+        System.out.println(result5);
+        System.out.println(a % 2);
+        System.out.println(b % 2);
+
+    }
+}
