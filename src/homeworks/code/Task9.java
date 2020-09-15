e7b291d912c82fb7f855a7abf6769855ffc07cf9
Natali <tvnatasha0310@gmail.com>
2020-09-15 18:22:31 +0200

    new

---
+++ src/homeworks/code/Task9.java
@@ -0,0 +1,16 @@
+package homeworks.code;
+
+public class Task9 {
+    public static void main(String [] args)  {
+        String a = "3\n";
+        String b = "2\n";
+        String c = "1";
+        String result = a +b +c;
+
+        System.out.println(result);
+        //System.out.println(b);
+        //System.out.println(c);
+        System.out.println("Поехали!!!");
+    }
+
+}
