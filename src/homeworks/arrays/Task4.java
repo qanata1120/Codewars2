999e256048347900505bb2977a15d7402bc7e87e
Natali <tvnatasha0310@gmail.com>
2020-09-22 15:09:04 +0200

    new

---
+++ src/homeworks/arrays/Task4.java
@@ -0,0 +1,19 @@
+package homeworks.arrays;
+
+public class Task4 {
+   /* Дан массив:
+    int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    необходимо вывести среднее арифметическое всех значений массива.*/
+
+    public static void main(String[]args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        double sum = 0;
+        for (int i = 0; i < array.length; i++){
+        sum = sum + array[i];     // the same sum += array[i];
+        }
+        System.out.println(sum);
+        int average = sum / array.length;
+        System.out.println(average);
+    }
+
+}
