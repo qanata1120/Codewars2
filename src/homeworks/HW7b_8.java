3b51ecbe9461cd85b292270247bc2b87115cda55
Natali <tvnatasha0310@gmail.com>
2020-09-28 13:16:02 +0200

    new

--- src/homeworks/HW7b_8.java
+++ src/homeworks/HW7b_8.java
@@ -1,8 +1,5 @@
 package homeworks;
 
-
-
-
 public class HW7b_8 {
     //Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
 //The output should be two capital letters with a dot separating them.
