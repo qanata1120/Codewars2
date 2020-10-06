475a9b6baae0100b858b3470ade1220da9a1bb42
Natali <tvnatasha0310@gmail.com>
2020-10-06 14:56:19 +0200

    refactoring

---
+++ src/lesson_if_else/If_Else.java
@@ -0,0 +1,31 @@
+package lesson_if_else;
+
+
+//1. Find is it more numbers can be devided by 3 or by 5 in between rage on numbers 1-100.
+
+public class If_Else {
+    public static void main(String[] args) {
+        int count1 = 0;
+        int count2 = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                count1++;
+            }
+            if (i % 5 == 0) {
+                count2++;
+            }
+        }
+        System.out.println(count1);
+        System.out.println(count2);
+
+        if (count1 > count2) {
+            System.out.println(count1 + " more");
+        } else {
+            if (count2 > count1) {
+                System.out.println(count2 + " more");
+            } else {
+                System.out.println("equal");
+            }
+        }
+    }
+}
